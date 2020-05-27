7cbe11552da4357a789037af019342a13aba5a4f
Bravizin <leo_rse@hotmail.com>
2020-05-27 00:49:19 -0300

    Project created

---
+++ src/main/java/br/com/voxus/paymentposting/PaymentPostingApplication.java
@@ -0,0 +1,13 @@
+package br.com.voxus.paymentposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/voxus/paymentposting/PaymentPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.voxus.paymentposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
